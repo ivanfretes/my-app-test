906f26300aa0d5722f96fa91521fa7bb5ebf4309
Ivan Fretes <vanfretes@gmail.com>
2024-12-19 15:47:49 -0300

    initial commit

---
+++ app.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <!--NuxtRouteAnnouncer />
+    <NuxtWelcome /-->
+    
+
+    <form  @submit="submit">
+      Name
+      <input type="text" name="name"/><br>
+      Email
+      <input type="email" name="name"/><br>
+
+      <button type="submit" >Save</button>
+    </form>
+
+
+    
+  </div>
+</template>
+
+
+<script setup>
+
+
+  const submit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
+</script>
